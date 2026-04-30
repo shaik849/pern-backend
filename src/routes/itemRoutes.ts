fbab6d9b556476378a51d5df09afdc57e07800a7
Sanavulla Shaik <110591352+shaik849@users.noreply.github.com>
2026-04-30 12:57:58 +0530

    Change GET route from '/posts' to '/post'

--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -4,7 +4,7 @@ import { itemController } from "../controllers/itemController"
 const router = Router();
 
 router.post('/post', (req: Request, res: Response) => itemController.create(req, res));
-router.get('/posts', (req: Request, res: Response) => itemController.getAll(req, res));
+router.get('/post', (req: Request, res: Response) => itemController.getAll(req, res));
 router.get('/post/:id', (req: Request, res: Response) => itemController.getById(req, res));
 router.put('/post/:id', (req: Request, res: Response) => itemController.update(req, res));
 router.delete('/post/:id', (req: Request, res: Response) => itemController.delete(req, res));
