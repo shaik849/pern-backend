bf4e74945abe7cf4d7d03cab0da804d2021d6d41
Sanavulla Shaik <110591352+shaik849@users.noreply.github.com>
2026-04-30 20:44:49 +0530

    Log the server port to the console

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
-
+console.log(port)
 app.use(cors({
     origin: '*' // Allows all origins
   }));
